fb8eb56d79db9573c8606d514cd23fe048d1a54f
andrei.kondratev <andrei.kondratev@unimarket.com>
2022-04-05 17:31:58 +1200

    Set up quick start project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- This one is reported by lint, but works fine in the app: -->
+  <h1>{{ $t('announcements.test') }}</h1>
+  <h1>{{ $t('announcements.testNz') }}</h1>
+  <h1>{{ $t('announcements.testAu') }}</h1>
+  <h1>{{ $t('announcements.testUs') }}</h1>
+  <h1>{{ $t('anotherMessage') }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from "vue-i18n";
 
-createApp(App).mount('#app')
+export const i18n = createI18n({
+    globalInjection: true,
+    locale: "en-NZ",
+    messages: {
+        "en-NZ": require("@/locales/en-NZ.json")
+    }
+});
+
+createApp(App).use(i18n).mount('#app')
